82831d86d71b7608a7ec997ca7e8c24df9385e8a
nicolettiant89 <nicolettiant89@gmail.com>
2023-05-16 17:04:19 +0200

    sdfsfsf

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@
 		// Title Bar.
 			$(
 				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
+					
 					'<span class="title">' + $('#logo').html() + '</span>' +
 				'</div>'
 			)
